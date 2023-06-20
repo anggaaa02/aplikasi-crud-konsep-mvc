b2c4f4528205ff3aea8f7ac1fafd2fd24808be81
anggaaa02 <127072286+anggaaa02@users.noreply.github.com>
2023-06-20 10:53:07 +0700

    first commit

---
+++ layouts/guest.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>TUGAS 2 | PBWL</title>
+
+	<link rel="shortcut icon" href="<?php echo AST; ?>/img/favicon.ico" type="image/x-icon">
+	<style>
+		body {
+			font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    		margin: 0 100px;
+    		background-color: #b1c5e4;
+		}
+
+		.info {
+			background-color: #8aa7d4;
+    		padding: 20px;
+    		margin-top: 10px;
+		}
+	</style>
+</head>
+
+<body>
+	<?php require_once ROOT . "app/views/" . $view . ".php"; ?>
+</body>
+
+</html>
